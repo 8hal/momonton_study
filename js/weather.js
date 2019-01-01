22d717c629429013b68f58c303cedb29f16c6d08
8hal <mkm8421@gmail.com>
2019-01-01 10:15:50 +0900

    add questions
    read all codes and added questions about everything I didn't know.

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,18 @@
 const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+//[question bg_js_01] how can i get API KEY for weather app?
 const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
 
 const weather = document.querySelector(".js-weather .weather__text");
-
+//[question bg_js_02] what happend i use querySelector with 2 selectors?
 function getWeather(coords) {
   fetch(
-    `${WEATHER_API}lat=${coords.lat}&lon=${
-      coords.lng
-    }&appid=${API_KEY}&units=metric`
+    //[question bg_js_03] what is the fetch?
+    `${WEATHER_API}lat=${coords.lat}&lon=${coords.lng}&appid=${API_KEY}&units=metric`
+    //[question bg_js_04] what is the units=metric?
   )
     .then(response => response.json())
+    //[question bg_js_05] what is the .then?
+    //[question bg_js_06] what is the =>?
     .then(json => {
       const name = json.name;
       const temperature = json.main.temp;
@@ -25,6 +28,7 @@ function handleGeoSuccess(position) {
     lng
   };
   localStorage.setItem("coords", JSON.stringify(coords));
+  //[question bg_js_08] what is the JSON.stringify()?
   getWeather(coords);
 }
 
@@ -36,10 +40,12 @@ function loadWeather() {
   const currentCoords = localStorage.getItem("coords");
   if (currentCoords !== null) {
     const parsedCoords = JSON.parse(currentCoords);
+    //[question bg_js_07] what is the JSON.parse()?
     getWeather(parsedCoords);
     return;
   } else {
     navigator.geolocation.getCurrentPosition(
+    //[question bg_js_08] what is the navigator.geolocation.getCurrentPosition?
       handleGeoSuccess,
       handleGeoFailure
     );
